cf728a95082bfa479918561c565f8fa63294204f
Steve Hellier <steve_hellier@hotmail.com>
2021-03-26 13:09:38 +0000

    Update pimpmykali.sh

--- pimpmykali.sh
+++ pimpmykali.sh
@@ -433,7 +433,7 @@ install_vscode () {
     echo -e "\n  $greenplus installing vscode"
     eval curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
     eval mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-    eval echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list
+    eval echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
     eval apt update && apt install code
     }
 
